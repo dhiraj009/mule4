f188c9d715262119593b08b9ec5ad9741a90ffac
Dhirajkumar.Singh <Dhirajkumar.Singh@INCTN263.Incture.local>
2020-05-16 20:47:52 +0530

    Crypto + Java Invoke functions

---
+++ crypto-invoke-java-example/src/main/java/com/example/AppendData.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class AppendData {
+      public static String appendHello(String name) {  //method should be static to invoke from dwl
+    	  return "Hello " + name ;
+      }
+      
+      public String appendName(String fname , String lname) {
+    	  
+      }
+}
